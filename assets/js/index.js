d1c4495c94d391c223be3cee488a38b3e16ea1b6
Edriss563 <edrissalokozay77@gmail.com>
2022-02-16 11:30:34 -0800

    working on sticky navbar

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+/* jquery */
+$(document).ready(function () {
+    $('.about').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+});
