14db88a17e0509f83ab1c5e82c1c87510f52ee60
Edriss563 <edrissalokozay77@gmail.com>
2022-02-19 11:37:58 -0800

    working on animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,47 @@
-// /* jquery */
-// $(document).ready(function() {
+/* jquery */
+$(document).ready(function () {
 
-// /*--- Sticky navigation ---*/
-// $('.about').waypoint(function(direction){
-//     if(direction == "down"){
-//         $('nav').addClass('sticky');
-//     }else{
-//         $('nav').removeClass('sticky');
-//     }
-// },{
-//     offset: '60px;'
-// });
+    /*--- Sticky navigation ---*/
+    $('#about').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
 
-// });
+    /*--- navigation scroll ---*/
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 60
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+     /*--- animation on scroll ---*/
+     $('#about').waypoint(function (direction) {
+        $('#image').addClass('animated fadeInLeft'); 
+        $('#text').addClass('animated fadeInRight'); 
+    }, {
+        offset: '50%'
+    });
+
+    // for skills progress
+     $('#skills').waypoint(function (direction) {
+        $('#progress').addClass('.progress-animation'); 
+    });
+
+});
+
 
 
